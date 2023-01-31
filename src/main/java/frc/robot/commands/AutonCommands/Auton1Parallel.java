fd8756515acf52ef363d8cdc0a1f19123ba2b3e1
TroBots <whitmore.robot.frc@outlook.com>
2023-01-30 20:41:56 -0500

    VH - formatted auto

--- src/main/java/frc/robot/commands/AutonCommands/Auton1Parallel.java
+++ src/main/java/frc/robot/commands/AutonCommands/Auton1Parallel.java
@@ -1,4 +1,5 @@
 package frc.robot.commands.AutonCommands;
+
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
@@ -13,30 +14,27 @@ import frc.robot.commands.lightingCommands.*;
  */
 public class Auton1Parallel extends ParallelCommandGroup {
 
-
-    public Auton1Parallel(){
+    public Auton1Parallel() {
 
     }
 
-        public Auton1Parallel(double targetRotPos, double targetExtendPos) {
+    public Auton1Parallel(double targetRotPos, double targetExtendPos) {
 
         addCommands(new cmdSetArmRotPos(targetRotPos, 30, .15, false));
-        addCommands(new cmdSetArmExtendPos(targetExtendPos,30,0.15,false));
+        addCommands(new cmdSetArmExtendPos(targetExtendPos, 30, 0.15, false));
 
     }
 
-    public Auton1Parallel(double targetRotPos, double targetExtendPos, double targetDistance, double driveSpeed, boolean driveStraight) {
-//if drive straight false ==> it will strafe
- this(targetRotPos,targetExtendPos);
+    public Auton1Parallel(double targetRotPos, double targetExtendPos, double targetDistance, double driveSpeed,
+            boolean driveStraight) {
+        // if drive straight false ==> it will strafe
+        this(targetRotPos, targetExtendPos);
 
-    if(driveStraight){
-      //  addCommands(new cmdDriveStraight(targetDistance,driveSpeed));
+        if (driveStraight) {
+            // addCommands(new cmdDriveStraight(targetDistance,driveSpeed));
+        } else {
+            // addCommands(new cmdStrafe(targetDistance, driveSpeed));
+        }
     }
-    else{
-      //  addCommands(new cmdStrafe(targetDistance, driveSpeed));
-    }
-    }
-
-    
 
 }
