c0e81f66092492fffa286df144b452750dd43713
TroBots <whitmore.robot.frc@outlook.com>
2023-01-30 20:02:51 -0500

    VH eddited auton1

--- src/main/java/frc/robot/commands/AutonCommands/Auton1.java
+++ src/main/java/frc/robot/commands/AutonCommands/Auton1.java
@@ -21,7 +21,7 @@ public class Auton1 extends SequentialCommandGroup {
     public Auton1() {
 
         addCommands(new cmdSetArmRotPos(Arm.carryRotPos, 30, .15, false));
-        addCommands(new Auton1Parallel(Arm.deliveryRotPos,Arm.deliveryExtendPos,0,0,true));
+        addCommands(new Auton1Parallel(Arm.deliveryRotPos,Arm.deliveryExtendPos));
 
         addCommands(new cmdSetGripperPos(Gripper.openPos));
 
